e4b62f82318d459c44de84060c938e21f8d1b971
Rupesh Raghavan <rupesh_raghavan@apple.com>
2024-05-13 16:28:19 -0700

    add metrics server

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/rupeshtr78/nvidia-metrics/api"
 	nvidiametrics "github.com/rupeshtr78/nvidia-metrics/internal/nvidia-metrics"
 	prometheusmetrics "github.com/rupeshtr78/nvidia-metrics/internal/prometheus_metrics"
 	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
@@ -25,8 +26,8 @@ func main() {
 	}
 
 	// run the metrics server
-	// api.RunMetrics()
-	RunMetricsLocal()
+	api.RunMetrics()
+	// RunMetricsLocal()
 
 }
 
