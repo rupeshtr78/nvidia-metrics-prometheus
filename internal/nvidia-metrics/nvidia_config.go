a7a741f44a7b82237e31861b3fb1ba90a76e51e5
rupeshtr78 <rupeshtr78@gmail.com>
2024-05-19 19:30:00 -0700

    refactor: Remove unused code and update GPU metrics collection

--- internal/nvidia-metrics/nvidia_config.go
+++ internal/nvidia-metrics/nvidia_config.go
@@ -56,3 +56,57 @@ func GetLabelKeys(metricName string) map[string]string {
 	return labelKeys
 
 }
+
+type LabelFunction struct {
+	LabelFunc map[string]func(nvml.Device) (any, nvml.Return)
+}
+
+// func GetNvidiaLabels(handle nvml.Device) {
+
+// handle.GetPciInfo()
+// handle.GetTemperatureThreshold(nvml.TEMPERATURE_THRESHOLD_SLOWDOWN)
+// 	handle.GetTemperatureThreshold(nvml.TEMPERATURE_THRESHOLD_SHUTDOWN)
+// 	handle.GetClockInfo(nvml.CLOCK_GRAPHICS)
+// 	handle.GetClockInfo(nvml.CLOCK_SM)
+// 	handle.GetClockInfo(nvml.CLOCK_MEM)
+// 	handle.GetClockInfo(nvml.CLOCK_VIDEO)
+// 	handle.GetClockInfo(nvml.CLOCK_COUNT)
+// handle.GetSupportedVgpus()
+// 	handle.GetVgpuMetadata()
+// 	handle.GetVgpuUtilization()
+// 	handle.GetVgpuProcessUtilization()
+// 	handle.GetAttributes()
+// 	handle.GetEncoderCapacity()
+// 	handle.GetEncoderStats()
+// 	handle.GetActiveVgpus()
+// 	handle.GetComputeMode()
+// 	handle.GetEccMode()
+// 	handle.GetTotalEccErrors()
+// 	handle.GetDetailedEccErrors()
+// 	handle.GetMemoryErrorCounter()
+// 	handle.GetVbiosVersion()
+// 	handle.GetBridgeChipInfo()
+// 	handle.GetInforomVersion(nvml.INFOROM_OEM)
+// 	handle.GetInforomVersion(nvml.INFOROM_ECC)
+// 	handle.GetInforomVersion(nvml.INFOROM_POWER)
+// 	handle.GetInforomVersion(nvml.INFOROM_COUNT)
+// 	handle.GetInforomImageVersion()
+// 	handle.GetMaxClockInfo(nvml.CLOCK_GRAPHICS)
+// 	handle.GetMaxClockInfo(nvml.CLOCK_SM)
+// 	handle.GetMaxClockInfo(nvml.CLOCK_MEM)
+// 	handle.GetMaxClockInfo(nvml.CLOCK_VIDEO)
+// 	handle.GetMaxClockInfo(nvml.CLOCK_COUNT)
+// 	handle.GetApplicationsClock(nvml.CLOCK_GRAPHICS)
+// 	handle.GetApplicationsClock(nvml.CLOCK_SM)
+// 	handle.GetApplicationsClock(nvml.CLOCK_MEM)
+// 	handle.GetApplicationsClock(nvml.CLOCK_VIDEO)
+// 	handle.GetApplicationsClock(nvml.CLOCK_COUNT)
+// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_GRAPHICS)
+// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_SM)
+// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_MEM)
+// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_VIDEO)
+// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_COUNT)
+// 	handle.GetCudaComputeCapability()
+// 	handle.GetNvLinkCapability()
+// 	handle.GetNvLinkRemotePciInfo()
+// }
