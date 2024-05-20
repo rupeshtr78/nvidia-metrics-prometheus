527b77a314cace440671b3398913d051791f8965
Rupesh Raghavan <rupesh_raghavan@apple.com>
2024-05-20 10:32:14 -0700

    nvidia labels

--- internal/nvidia-metrics/nvidia_config.go
+++ internal/nvidia-metrics/nvidia_config.go
@@ -2,7 +2,6 @@ package nvidiametrics
 
 import (
 	"github.com/NVIDIA/go-nvml/pkg/nvml"
-	prometheusmetrics "github.com/rupeshtr78/nvidia-metrics/internal/prometheus_metrics"
 	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
 	"go.uber.org/zap"
 )
@@ -16,6 +15,21 @@ type GPUDeviceMetrics struct {
 	GPUPowerUsage       float64
 	GPURunningProcesses int
 }
+
+// Metrics
+const (
+	gpu_cpu_utilization = "gpu_cpu_utilization"
+	gpu_mem_utilization = "gpu_mem_utilization"
+	gpu_power_usage     = "gpu_power_usage"
+	gpu_running_process = "gpu_running_process"
+	gpu_temperature     = "gpu_temperature"
+)
+
+// Labels
+const (
+	gpu_id   = "gpu_id"
+	gpu_name = "gpu_name"
+)
 
 // InitNVML initializes the NVML library.
 func InitNVML() {
@@ -32,81 +46,3 @@ func ShutdownNVML() {
 	}
 	logger.Info("Shutdown NVML")
 }
-
-// GetLabelKeys returns the label keys for the given metric name.
-// Example: "key":"gpu_power_usage","label":{"label1":"gpu_id","label2":"gpu_name"}}
-func GetLabelKeys(metricName string) map[string]string {
-	labelKeys := make(map[string]string)
-
-	if _, ok := prometheusmetrics.RegisteredLabels[metricName]; !ok {
-		logger.Warn("Metric not found in registered labels", zap.String("metric_name", metricName))
-		return labelKeys
-	}
-
-	keys := prometheusmetrics.RegisteredLabels[metricName]
-	// iterate over the keys and add label name to the map
-	for i, key := range keys {
-		if len(key) == 0 {
-			continue
-		}
-		// adding dummy values for now will be updates while setting guage
-		labelKeys[key] = i
-	}
-
-	return labelKeys
-
-}
-
-type LabelFunction struct {
-	LabelFunc map[string]func(nvml.Device) (any, nvml.Return)
-}
-
-// func GetNvidiaLabels(handle nvml.Device) {
-
-// handle.GetPciInfo()
-// handle.GetTemperatureThreshold(nvml.TEMPERATURE_THRESHOLD_SLOWDOWN)
-// 	handle.GetTemperatureThreshold(nvml.TEMPERATURE_THRESHOLD_SHUTDOWN)
-// 	handle.GetClockInfo(nvml.CLOCK_GRAPHICS)
-// 	handle.GetClockInfo(nvml.CLOCK_SM)
-// 	handle.GetClockInfo(nvml.CLOCK_MEM)
-// 	handle.GetClockInfo(nvml.CLOCK_VIDEO)
-// 	handle.GetClockInfo(nvml.CLOCK_COUNT)
-// handle.GetSupportedVgpus()
-// 	handle.GetVgpuMetadata()
-// 	handle.GetVgpuUtilization()
-// 	handle.GetVgpuProcessUtilization()
-// 	handle.GetAttributes()
-// 	handle.GetEncoderCapacity()
-// 	handle.GetEncoderStats()
-// 	handle.GetActiveVgpus()
-// 	handle.GetComputeMode()
-// 	handle.GetEccMode()
-// 	handle.GetTotalEccErrors()
-// 	handle.GetDetailedEccErrors()
-// 	handle.GetMemoryErrorCounter()
-// 	handle.GetVbiosVersion()
-// 	handle.GetBridgeChipInfo()
-// 	handle.GetInforomVersion(nvml.INFOROM_OEM)
-// 	handle.GetInforomVersion(nvml.INFOROM_ECC)
-// 	handle.GetInforomVersion(nvml.INFOROM_POWER)
-// 	handle.GetInforomVersion(nvml.INFOROM_COUNT)
-// 	handle.GetInforomImageVersion()
-// 	handle.GetMaxClockInfo(nvml.CLOCK_GRAPHICS)
-// 	handle.GetMaxClockInfo(nvml.CLOCK_SM)
-// 	handle.GetMaxClockInfo(nvml.CLOCK_MEM)
-// 	handle.GetMaxClockInfo(nvml.CLOCK_VIDEO)
-// 	handle.GetMaxClockInfo(nvml.CLOCK_COUNT)
-// 	handle.GetApplicationsClock(nvml.CLOCK_GRAPHICS)
-// 	handle.GetApplicationsClock(nvml.CLOCK_SM)
-// 	handle.GetApplicationsClock(nvml.CLOCK_MEM)
-// 	handle.GetApplicationsClock(nvml.CLOCK_VIDEO)
-// 	handle.GetApplicationsClock(nvml.CLOCK_COUNT)
-// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_GRAPHICS)
-// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_SM)
-// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_MEM)
-// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_VIDEO)
-// 	handle.GetDefaultApplicationsClock(nvml.CLOCK_COUNT)
-// 	handle.GetCudaComputeCapability()
-// 	handle.GetNvLinkCapability()
-// 	handle.GetNvLinkRemotePciInfo()
-// }
---
+++ internal/nvidia-metrics/nvidia_labels.go
@@ -0,0 +1,109 @@
+package nvidiametrics
+
+import (
+	"fmt"
+
+	"github.com/NVIDIA/go-nvml/pkg/nvml"
+	prometheusmetrics "github.com/rupeshtr78/nvidia-metrics/internal/prometheus_metrics"
+	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
+	"go.uber.org/zap"
+)
+
+// DeviceInfoRetriever: A function type for retrieving device information.
+type DeviceInfoRetriever func(device nvml.Device) (any, nvml.Return)
+
+type LabelFunctions map[string]DeviceInfoRetriever
+
+func NewLabelFunction() *LabelFunctions {
+	l := make(LabelFunctions)
+	return &l
+}
+
+func NewDeviceInfoRetriever(f func(device nvml.Device) (any, nvml.Return)) DeviceInfoRetriever {
+	return DeviceInfoRetriever(f)
+}
+
+func (lf *LabelFunctions) AddLabel(labelName string, f DeviceInfoRetriever) {
+	(*lf)[labelName] = f
+}
+
+func (lf *LabelFunctions) GetLabelFunc(labelName string) (DeviceInfoRetriever, error) {
+	if f, ok := (*lf)[labelName]; ok {
+		return f, nil
+	}
+	return nil, fmt.Errorf("label function not found")
+}
+
+func (lf *LabelFunctions) SetLabelFunc(labelName string, f DeviceInfoRetriever) {
+	(*lf)[labelName] = f
+}
+
+func (lf *LabelFunctions) FetchDeviceLabelValue(device nvml.Device, labelName string) any {
+
+	labelFunc, err := lf.GetLabelFunc(labelName)
+	if err != nil {
+		logger.Error("Error fetching label function", zap.String("label_name", labelName))
+		return nil
+	}
+
+	value, ret := labelFunc(device)
+	if ret != nvml.SUCCESS {
+		logger.Error("Error fetching label value", zap.String("label_name", labelName))
+		return nil
+	}
+	return value
+
+}
+
+// example usage
+func GetLableValue(device nvml.Device, name string) {
+
+	labelFunc := NewLabelFunction()
+	labelFunc.AddLabel(gpu_id, NewDeviceInfoRetriever(func(device nvml.Device) (any, nvml.Return) {
+		return device.GetIndex()
+	}))
+
+	// get the label value
+	value := labelFunc.FetchDeviceLabelValue(device, name)
+	_ = value
+
+}
+
+// GetLabelKeys returns the label keys for the given metric name.
+// Example: "key":"gpu_power_usage","label":{"label1":"gpu_id","label2":"gpu_name"}}
+func GetLabelKeys(metricName string) map[string]string {
+	labelKeys := make(map[string]string)
+
+	if _, ok := prometheusmetrics.RegisteredLabels[metricName]; !ok {
+		logger.Warn("Metric not found in registered labels", zap.String("metric_name", metricName))
+		return labelKeys
+	}
+
+	keys := prometheusmetrics.RegisteredLabels[metricName]
+	// iterate over the keys and add label name to the map
+	for i, key := range keys {
+		if len(key) == 0 {
+			continue
+		}
+		// adding dummy values for now will be updates while setting guage
+		labelKeys[key] = i
+	}
+
+	return labelKeys
+
+}
+
+// Metrics
+const (
+	gpu_cpu_utilization = "gpu_cpu_utilization"
+	gpu_mem_utilization = "gpu_mem_utilization"
+	gpu_power_usage     = "gpu_power_usage"
+	gpu_running_process = "gpu_running_process"
+	gpu_temperature     = "gpu_temperature"
+)
+
+// Labels
+const (
+	gpu_id   = "gpu_id"
+	gpu_name = "gpu_name"
+)
