7783afd1d636949ebd526287a0dfe8755ab29c21
rupeshtr78 <rupeshtr78@gmail.com>
2024-06-19 10:44:19 -0700

    nvidia-test ginkgo

--- internal/nvidia-metrics/nvml_init_test.go
+++ internal/nvidia-metrics/nvml_init_test.go
@@ -3,17 +3,28 @@ package nvidiametrics_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"go.uber.org/zap/zapcore"
 
 	nvidiametrics "github.com/rupeshtr78/nvidia-metrics/internal/nvidia-metrics"
 	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
 )
 
+type stubLogger struct {
+	fatalCalled bool
+}
+
+func (s *stubLogger) Fatal(msg string, fields ...zapcore.Field) {
+	s.fatalCalled = true
+}
+
 var _ = Describe("NvmlInit", func() {
 	Context("When Init is called", func() {
 		It("should initialize NVML", func() {
+			stub := &stubLogger{}
+			logger.GetLogger("debug", false, "")
 			nvidiametrics.InitNVML()
 			// Expect no fatal errors
-			Expect(logger.Fatal).To(BeNil())
+			Expect(stub.fatalCalled).To(BeFalse())
 
 		})
 	})
