2b5ca7b846cce9313b51adbbfcf97f1d62b47c4d
rupeshtr78 <rupeshtr78@gmail.com>
2024-06-19 10:26:27 -0700

    nvidia-test ginkgo

--- internal/nvidia-metrics/nvml_init_test.go
+++ internal/nvidia-metrics/nvml_init_test.go
@@ -1,31 +1,20 @@
-package nvidiametrics
+package nvidiametrics_test
 
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
-	"go.uber.org/zap"
 
-	nvml "github.com/NVIDIA/go-nvml/pkg/nvml"
+	nvidiametrics "github.com/rupeshtr78/nvidia-metrics/internal/nvidia-metrics"
+	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
 )
 
-var _ = Describe("ShutdownNVML", func() {
-	var (
-		err error
-	)
+var _ = Describe("NvmlInit", func() {
+	Context("When Init is called", func() {
+		It("should initialize NVML", func() {
+			nvidiametrics.InitNVML()
+			// Expect no fatal errors
+			Expect(logger.Fatal).To(BeNil())
 
-	JustBeforeEach(func() {
-		err = nvml.Shutdown()
-		if err != nil && err != nvml.SUCCESS {
-			logger.Fatal("Failed to shutdown NVML", zap.Error(err))
-		} else {
-			logger.Info("Shutdown NVML Successfully")
-		}
-	})
-
-	Context("when calling Shutdown", func() {
-		It("should not return an error", func() {
-			Expect(err).NotTo(HaveOccurred())
 		})
 	})
 
