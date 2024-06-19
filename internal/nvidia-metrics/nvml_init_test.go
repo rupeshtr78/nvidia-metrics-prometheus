4d589db7180d7ba899d7966c24539c457873832c
rupeshtr78 <rupeshtr78@gmail.com>
2024-06-19 11:49:50 -0700

    add ginkgo

--- internal/nvidia-metrics/nvml_init_test.go
+++ internal/nvidia-metrics/nvml_init_test.go
@@ -3,7 +3,6 @@ package nvidiametrics_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"go.uber.org/zap/zapcore"
 
 	nvidiametrics "github.com/rupeshtr78/nvidia-metrics/internal/nvidia-metrics"
 	"github.com/rupeshtr78/nvidia-metrics/pkg/logger"
@@ -13,26 +12,24 @@ type stubLogger struct {
 	fatalCalled bool
 }
 
-func (s *stubLogger) Fatal(msg string, fields ...zapcore.Field) {
-	s.fatalCalled = true
-}
+var _ = logger.GetLogger("debug", false, "")
 
 var _ = Describe("NvmlInit", func() {
 	Context("When Init is called", func() {
-		// if NVML is not initialized, it should call logger.Fatal
-		It("should call logger.Fatal if NVML is not initialized", func() {
-			stub := &stubLogger{}
-			logger.GetLogger("debug", false, "")
-			nvidiametrics.InitNVML()
-			// Expect no fatal errors
-			Expect(stub.fatalCalled).To(BeTrue())
 
-		})
 		// if NVML is initialized, it should not call logger.Fatal
 		It("should initialize NVML", func() {
 			stub := &stubLogger{}
-			logger.GetLogger("debug", false, "")
 			nvidiametrics.InitNVML()
+			// Expect no fatal errors
+			Expect(stub.fatalCalled).To(BeFalse())
+
+		})
+
+		// // if NVML is not initialized, it should call logger.Fatal
+		It("should shutdown NVML", func() {
+			stub := &stubLogger{}
+			nvidiametrics.ShutdownNVML()
 			// Expect no fatal errors
 			Expect(stub.fatalCalled).To(BeFalse())
 
